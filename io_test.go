38369d7a68805f48121c3faa01fed11fc7b6bb61
Lukas Herman <lherman.cs@gmail.com>
2020-04-10 11:08:14 -0400

    Add unit test for WriterSync

--- io_test.go
+++ io_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -19,5 +22,35 @@ func TestCopyMultiLine(t *testing.T) {
 	out := writer.String()
 	if out != expected {
 		t.Fatalf("expected:\n%s\ngot:\n%s", expected, out)
+	}
+}
+
+func TestWriterSync(t *testing.T) {
+	var buf bytes.Buffer
+
+	w := WriterSync(&buf)
+	var wg sync.WaitGroup
+
+	writeAsync := func(t string) {
+		io.WriteString(w, t)
+		wg.Done()
+	}
+
+	testcases := 1000
+	wg.Add(testcases)
+	expected := make(map[string]struct{})
+	for i := 0; i < testcases; i++ {
+		word := fmt.Sprintf("test%d", i)
+		expected[word] = struct{}{}
+		go writeAsync(word + ",")
+	}
+	wg.Wait()
+
+	out := buf.String()
+	words := strings.Split(out, ",")
+	for _, word := range words[:len(words)-1] {
+		if _, ok := expected[word]; !ok {
+			t.Fatalf("expected the output to contain %s", word)
+		}
 	}
 }
