9e87593f1e26c66c81856cd23bb8affb0a58f83f
Lukas Herman <lherman.cs@gmail.com>
2020-04-12 16:19:22 -0400

    Fix wrong wording

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ import (
 func TestStartWithNoConfigs(t *testing.T) {
 	code := Start()
 	if code == 0 {
-		t.Fatalf("expected program to exit with 0, but got %d", code)
+		t.Fatalf("expected program to exit with non-zero, but got %d", code)
 	}
 }
 
