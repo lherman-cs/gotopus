1e0430ce880cfc9a52f927d503c6a2413e020ee1
Lukas Herman <lherman.cs@gmail.com>
2020-04-12 16:22:06 -0400

    Add main unit test when there's an error

--- main_test.go
+++ main_test.go
@@ -42,3 +42,34 @@ jobs:
 		t.Fatalf("expected program to exit with 0, but got %d", code)
 	}
 }
+
+func TestStartWithError(t *testing.T) {
+	tmp, err := ioutil.TempFile("", "test_*.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		tmp.Close()
+		os.Remove(tmp.Name())
+	}()
+
+	yamlStr := `
+jobs:
+  job1:
+    needs:
+      - job2
+  job2:
+    needs:
+      - job1`
+
+	_, err = io.Copy(tmp, strings.NewReader(yamlStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.Args = []string{"fake", tmp.Name()}
+	code := Start()
+	if code == 0 {
+		t.Fatalf("expected program to exit with non-zero, but got %d", code)
+	}
+}
